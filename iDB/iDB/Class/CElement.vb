05d0aca768a5113a4c54b29ad73cf92cc5d33c12
ALPHA\ritsuki <rit750104@gmail.com>
2017-01-26 15:14:41 +0900

    新規作成

---
+++ iDB/iDB/Class/CElement.vb
@@ -0,0 +1,32 @@
+﻿
+''' <summary>
+''' コンボボックスに表示用mの次列と内部IDを保持させるクラス
+''' </summary>
+<System.Diagnostics.DebuggerStepThrough>
+Public Class CElement
+
+	Private m_ID As Integer '内部ID
+	Private m_Name As String    '表示用文字列
+
+	''' <summary>
+	''' 表示用文字列はToStringをオーバーライドして取得する
+	''' </summary>
+	''' <returns></returns>
+	Public Overrides Function ToString() As String
+		Return NAME
+	End Function
+
+	''' <summary>
+	''' 実際の値
+	''' (ValueMemberに設定する文字列と同名にする)
+	''' </summary>
+	''' <returns></returns>
+	Public Property ID() As Integer
+		Get
+			Return m_ID
+		End Get
+		Set(value As Integer)
+			m_ID = value
+		End Set
+	End Property
+End Class
