e06af1e4894b16d86296a115498d9cb96b2e5221
ALPHA\ritsuki <asanuma-ritsuki@infomage.jp>
2017-01-27 11:37:01 +0900

    更新

--- PDFViewTest/PDFViewTest/Form1.Designer.vb
+++ PDFViewTest/PDFViewTest/Form1.Designer.vb
@@ -25,18 +25,21 @@ Partial Class Form1
 		Me.txtPage = New System.Windows.Forms.TextBox()
 		Me.btnMove = New System.Windows.Forms.Button()
 		Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+		Me.btnClear = New System.Windows.Forms.Button()
+		Me.Panel1 = New System.Windows.Forms.Panel()
+		Me.Panel1.SuspendLayout()
 		Me.SuspendLayout()
 		'
 		'txtPage
 		'
-		Me.txtPage.Location = New System.Drawing.Point(12, 12)
+		Me.txtPage.Location = New System.Drawing.Point(3, 3)
 		Me.txtPage.Name = "txtPage"
 		Me.txtPage.Size = New System.Drawing.Size(100, 19)
 		Me.txtPage.TabIndex = 0
 		'
 		'btnMove
 		'
-		Me.btnMove.Location = New System.Drawing.Point(118, 10)
+		Me.btnMove.Location = New System.Drawing.Point(109, 1)
 		Me.btnMove.Name = "btnMove"
 		Me.btnMove.Size = New System.Drawing.Size(75, 23)
 		Me.btnMove.TabIndex = 1
@@ -45,11 +48,32 @@ Partial Class Form1
 		'
 		'WebBrowser1
 		'
-		Me.WebBrowser1.Location = New System.Drawing.Point(12, 39)
+		Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.WebBrowser1.Location = New System.Drawing.Point(0, 28)
 		Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
 		Me.WebBrowser1.Name = "WebBrowser1"
-		Me.WebBrowser1.Size = New System.Drawing.Size(984, 678)
+		Me.WebBrowser1.Size = New System.Drawing.Size(1008, 701)
 		Me.WebBrowser1.TabIndex = 2
+		'
+		'btnClear
+		'
+		Me.btnClear.Location = New System.Drawing.Point(190, 1)
+		Me.btnClear.Name = "btnClear"
+		Me.btnClear.Size = New System.Drawing.Size(75, 23)
+		Me.btnClear.TabIndex = 3
+		Me.btnClear.Text = "クリア"
+		Me.btnClear.UseVisualStyleBackColor = True
+		'
+		'Panel1
+		'
+		Me.Panel1.Controls.Add(Me.txtPage)
+		Me.Panel1.Controls.Add(Me.btnClear)
+		Me.Panel1.Controls.Add(Me.btnMove)
+		Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+		Me.Panel1.Location = New System.Drawing.Point(0, 0)
+		Me.Panel1.Name = "Panel1"
+		Me.Panel1.Size = New System.Drawing.Size(1008, 28)
+		Me.Panel1.TabIndex = 4
 		'
 		'Form1
 		'
@@ -57,16 +81,18 @@ Partial Class Form1
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
 		Me.ClientSize = New System.Drawing.Size(1008, 729)
 		Me.Controls.Add(Me.WebBrowser1)
-		Me.Controls.Add(Me.btnMove)
-		Me.Controls.Add(Me.txtPage)
+		Me.Controls.Add(Me.Panel1)
 		Me.Name = "Form1"
 		Me.Text = "Form1"
+		Me.Panel1.ResumeLayout(False)
+		Me.Panel1.PerformLayout()
 		Me.ResumeLayout(False)
-		Me.PerformLayout()
 
 	End Sub
 
 	Friend WithEvents txtPage As TextBox
 	Friend WithEvents btnMove As Button
 	Friend WithEvents WebBrowser1 As WebBrowser
+	Friend WithEvents btnClear As Button
+	Friend WithEvents Panel1 As Panel
 End Class
--- PDFViewTest/PDFViewTest/Form1.vb
+++ PDFViewTest/PDFViewTest/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form1
 
-	Dim strFileName As String = ""
-	Dim iPage As Integer = 1
+	Dim strFileName As String = "C:\開発\その他\機関リポジトリ\フェリス女学院大学\02_CiNiiデータ加工分\CD内容\AA11323958\img\11\KJ00005073204.pdf"
+	Dim iPage As Integer = 5
 
 	Private Sub btnMove_Click(sender As Object, e As EventArgs) Handles btnMove.Click
 
@@ -17,7 +17,7 @@
 
 	Private Sub PdfView(ByVal strFileName As String)
 
-		Me.WebBrowser1.AllowWebBrowserDrop = False
+		Me.WebBrowser1.AllowWebBrowserDrop = True
 		Me.WebBrowser1.IsWebBrowserContextMenuEnabled = False
 		Me.WebBrowser1.WebBrowserShortcutsEnabled = False
 
@@ -27,10 +27,28 @@
 	End Sub
 
 	Private Sub viewPage()
-
-		Me.WebBrowser1.Url = New Uri(Me.strFileName + "#toolbar=0&navpanes=0&view=FitH&scrollbar=1&page=" + Me.strFileName)
+		'Me.WebBrowser1.Url = Nothing
+		'Me.WebBrowser1.Url = New Uri(Me.strFileName + "#toolbar=0&navpanes=0&view=FitH&scrollbar=1&page=" + iPage)
+		'Me.WebBrowser1.Url = New Uri(Me.strFileName & "#page=5")
+		Me.WebBrowser1.Navigate(Me.strFileName & "#toolbar=0&navpanes=0&zoom=50&page=2")
 		Me.WebBrowser1.Refresh()
 
 	End Sub
 
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+		Me.WebBrowser1.AllowWebBrowserDrop = True
+		Me.WebBrowser1.IsWebBrowserContextMenuEnabled = False
+		Me.WebBrowser1.WebBrowserShortcutsEnabled = False
+
+		'Me.strFileName = strFileName
+		'viewPage()
+
+	End Sub
+
+	Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+		Me.WebBrowser1.Url = Nothing
+
+	End Sub
 End Class
