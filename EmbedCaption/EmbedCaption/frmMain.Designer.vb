d216cfca178dfad69fc0d1249d07ffd6a7571faf
淺沼律樹 <asanuma-ritsuki@infomage.jp>
2018-11-30 17:41:09 +0900

    update

---
+++ EmbedCaption/EmbedCaption/frmMain.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+	Inherits System.Windows.Forms.Form
+
+	'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Windows フォーム デザイナーで必要です。
+	Private components As System.ComponentModel.IContainer
+
+	'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+	'Windows フォーム デザイナーを使用して変更できます。  
+	'コード エディターを使って変更しないでください。
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+		Me.lstResult = New System.Windows.Forms.ListBox()
+		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+		Me.Label7 = New System.Windows.Forms.Label()
+		Me.numYStart = New System.Windows.Forms.NumericUpDown()
+		Me.Label5 = New System.Windows.Forms.Label()
+		Me.C1ColorPicker1 = New C1.Win.C1Input.C1ColorPicker()
+		Me.txtLogFolder = New System.Windows.Forms.TextBox()
+		Me.Label6 = New System.Windows.Forms.Label()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.btnRun = New System.Windows.Forms.Button()
+		Me.txtOutputFolder = New System.Windows.Forms.TextBox()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.txtParentFolder = New System.Windows.Forms.TextBox()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.txtCSVFile = New System.Windows.Forms.TextBox()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.GroupBox2.SuspendLayout()
+		Me.GroupBox1.SuspendLayout()
+		CType(Me.numYStart, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.C1ColorPicker1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'GroupBox2
+		'
+		Me.GroupBox2.Controls.Add(Me.lstResult)
+		Me.GroupBox2.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.GroupBox2.Location = New System.Drawing.Point(0, 171)
+		Me.GroupBox2.Name = "GroupBox2"
+		Me.GroupBox2.Size = New System.Drawing.Size(784, 390)
+		Me.GroupBox2.TabIndex = 3
+		Me.GroupBox2.TabStop = False
+		Me.GroupBox2.Text = "進捗状況"
+		'
+		'lstResult
+		'
+		Me.lstResult.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.lstResult.FormattingEnabled = True
+		Me.lstResult.ItemHeight = 17
+		Me.lstResult.Location = New System.Drawing.Point(3, 20)
+		Me.lstResult.Name = "lstResult"
+		Me.lstResult.Size = New System.Drawing.Size(778, 367)
+		Me.lstResult.TabIndex = 0
+		'
+		'GroupBox1
+		'
+		Me.GroupBox1.Controls.Add(Me.Label7)
+		Me.GroupBox1.Controls.Add(Me.numYStart)
+		Me.GroupBox1.Controls.Add(Me.Label5)
+		Me.GroupBox1.Controls.Add(Me.C1ColorPicker1)
+		Me.GroupBox1.Controls.Add(Me.txtLogFolder)
+		Me.GroupBox1.Controls.Add(Me.Label6)
+		Me.GroupBox1.Controls.Add(Me.Label4)
+		Me.GroupBox1.Controls.Add(Me.btnRun)
+		Me.GroupBox1.Controls.Add(Me.txtOutputFolder)
+		Me.GroupBox1.Controls.Add(Me.Label3)
+		Me.GroupBox1.Controls.Add(Me.txtParentFolder)
+		Me.GroupBox1.Controls.Add(Me.Label2)
+		Me.GroupBox1.Controls.Add(Me.txtCSVFile)
+		Me.GroupBox1.Controls.Add(Me.Label1)
+		Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top
+		Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+		Me.GroupBox1.Name = "GroupBox1"
+		Me.GroupBox1.Size = New System.Drawing.Size(784, 171)
+		Me.GroupBox1.TabIndex = 2
+		Me.GroupBox1.TabStop = False
+		Me.GroupBox1.Text = "出力設定"
+		'
+		'Label7
+		'
+		Me.Label7.Location = New System.Drawing.Point(541, 141)
+		Me.Label7.Name = "Label7"
+		Me.Label7.Size = New System.Drawing.Size(41, 25)
+		Me.Label7.TabIndex = 17
+		Me.Label7.Text = "Pixel"
+		Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		'
+		'numYStart
+		'
+		Me.numYStart.Location = New System.Drawing.Point(482, 143)
+		Me.numYStart.Maximum = New Decimal(New Integer() {1000, 0, 0, 0})
+		Me.numYStart.Name = "numYStart"
+		Me.numYStart.Size = New System.Drawing.Size(53, 24)
+		Me.numYStart.TabIndex = 16
+		Me.numYStart.Value = New Decimal(New Integer() {400, 0, 0, 0})
+		'
+		'Label5
+		'
+		Me.Label5.Location = New System.Drawing.Point(356, 141)
+		Me.Label5.Name = "Label5"
+		Me.Label5.Size = New System.Drawing.Size(120, 25)
+		Me.Label5.TabIndex = 15
+		Me.Label5.Text = "Y座標起点："
+		Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		'
+		'C1ColorPicker1
+		'
+		Me.C1ColorPicker1.AllowEmpty = False
+		Me.C1ColorPicker1.AutoSize = False
+		Me.C1ColorPicker1.Color = System.Drawing.Color.Gray
+		Me.C1ColorPicker1.ImagePadding = New System.Windows.Forms.Padding(0)
+		Me.C1ColorPicker1.Location = New System.Drawing.Point(138, 141)
+		Me.C1ColorPicker1.Name = "C1ColorPicker1"
+		Me.C1ColorPicker1.Size = New System.Drawing.Size(212, 25)
+		Me.C1ColorPicker1.TabIndex = 14
+		Me.C1ColorPicker1.Tag = Nothing
+		'
+		'txtLogFolder
+		'
+		Me.txtLogFolder.AllowDrop = True
+		Me.txtLogFolder.Location = New System.Drawing.Point(138, 111)
+		Me.txtLogFolder.Name = "txtLogFolder"
+		Me.txtLogFolder.Size = New System.Drawing.Size(528, 24)
+		Me.txtLogFolder.TabIndex = 11
+		'
+		'Label6
+		'
+		Me.Label6.Location = New System.Drawing.Point(12, 110)
+		Me.Label6.Name = "Label6"
+		Me.Label6.Size = New System.Drawing.Size(120, 25)
+		Me.Label6.TabIndex = 10
+		Me.Label6.Text = "ログフォルダ："
+		Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		'
+		'Label4
+		'
+		Me.Label4.Location = New System.Drawing.Point(12, 141)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(120, 25)
+		Me.Label4.TabIndex = 8
+		Me.Label4.Text = "色しきい値："
+		Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		'
+		'btnRun
+		'
+		Me.btnRun.Location = New System.Drawing.Point(697, 139)
+		Me.btnRun.Name = "btnRun"
+		Me.btnRun.Size = New System.Drawing.Size(75, 25)
+		Me.btnRun.TabIndex = 6
+		Me.btnRun.Text = "実　行"
+		Me.btnRun.UseVisualStyleBackColor = True
+		'
+		'txtOutputFolder
+		'
+		Me.txtOutputFolder.AllowDrop = True
+		Me.txtOutputFolder.Location = New System.Drawing.Point(138, 81)
+		Me.txtOutputFolder.Name = "txtOutputFolder"
+		Me.txtOutputFolder.Size = New System.Drawing.Size(528, 24)
+		Me.txtOutputFolder.TabIndex = 5
+		'
+		'Label3
+		'
+		Me.Label3.Location = New System.Drawing.Point(12, 80)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(120, 25)
+		Me.Label3.TabIndex = 4
+		Me.Label3.Text = "出力フォルダ："
+		Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		'
+		'txtParentFolder
+		'
+		Me.txtParentFolder.AllowDrop = True
+		Me.txtParentFolder.Location = New System.Drawing.Point(138, 51)
+		Me.txtParentFolder.Name = "txtParentFolder"
+		Me.txtParentFolder.Size = New System.Drawing.Size(528, 24)
+		Me.txtParentFolder.TabIndex = 3
+		'
+		'Label2
+		'
+		Me.Label2.Location = New System.Drawing.Point(12, 50)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(120, 25)
+		Me.Label2.TabIndex = 2
+		Me.Label2.Text = "親フォルダ："
+		Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		'
+		'txtCSVFile
+		'
+		Me.txtCSVFile.AllowDrop = True
+		Me.txtCSVFile.Location = New System.Drawing.Point(138, 21)
+		Me.txtCSVFile.Name = "txtCSVFile"
+		Me.txtCSVFile.Size = New System.Drawing.Size(528, 24)
+		Me.txtCSVFile.TabIndex = 1
+		'
+		'Label1
+		'
+		Me.Label1.Location = New System.Drawing.Point(12, 20)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(120, 25)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "CSVファイル："
+		Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		'
+		'frmMain
+		'
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+		Me.ClientSize = New System.Drawing.Size(784, 561)
+		Me.Controls.Add(Me.GroupBox2)
+		Me.Controls.Add(Me.GroupBox1)
+		Me.Font = New System.Drawing.Font("Meiryo UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+		Me.Name = "frmMain"
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "Form1"
+		Me.GroupBox2.ResumeLayout(False)
+		Me.GroupBox1.ResumeLayout(False)
+		Me.GroupBox1.PerformLayout()
+		CType(Me.numYStart, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.C1ColorPicker1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+
+	End Sub
+
+	Friend WithEvents GroupBox2 As GroupBox
+	Friend WithEvents lstResult As ListBox
+	Friend WithEvents GroupBox1 As GroupBox
+	Friend WithEvents C1ColorPicker1 As C1.Win.C1Input.C1ColorPicker
+	Friend WithEvents txtLogFolder As TextBox
+	Friend WithEvents Label6 As Label
+	Friend WithEvents Label4 As Label
+	Friend WithEvents btnRun As Button
+	Friend WithEvents txtOutputFolder As TextBox
+	Friend WithEvents Label3 As Label
+	Friend WithEvents txtParentFolder As TextBox
+	Friend WithEvents Label2 As Label
+	Friend WithEvents txtCSVFile As TextBox
+	Friend WithEvents Label1 As Label
+	Friend WithEvents Label7 As Label
+	Friend WithEvents numYStart As NumericUpDown
+	Friend WithEvents Label5 As Label
+End Class
---
+++ EmbedCaption/EmbedCaption/frmMain.vb
@@ -0,0 +1,195 @@
+﻿Public Class frmMain
+
+#Region "フォームイベント"
+
+	''' <summary>
+	''' フォームロード時
+	''' </summary>
+	''' <param name="sender"></param>
+	''' <param name="e"></param>
+	Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+		Me.Text = My.Application.Info.ProductName & " Ver." &
+			My.Application.Info.Version.ToString
+
+		XmlSettings.LoadFromXmlFile()
+		Me.txtCSVFile.Text = XmlSettings.Instance.CSVFile
+		Me.txtParentFolder.Text = XmlSettings.Instance.ParentFolder
+		Me.txtOutputFolder.Text = XmlSettings.Instance.OutputFolder
+		Me.txtLogFolder.Text = XmlSettings.Instance.LogFolder
+		Me.numYStart.Value = XmlSettings.Instance.YStart
+
+	End Sub
+
+	''' <summary>
+	''' フォームクロージング
+	''' </summary>
+	''' <param name="sender"></param>
+	''' <param name="e"></param>
+	Private Sub frmMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+		If MessageBox.Show("アプリケーションを終了します" & vbNewLine & "よろしいですか？", "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.No Then
+			e.Cancel = True
+		Else
+			XmlSettings.LoadFromXmlFile()
+			XmlSettings.Instance.CSVFile = Me.txtCSVFile.Text
+			XmlSettings.Instance.ParentFolder = Me.txtParentFolder.Text
+			XmlSettings.Instance.OutputFolder = Me.txtOutputFolder.Text
+			XmlSettings.Instance.LogFolder = Me.txtLogFolder.Text
+			XmlSettings.Instance.YStart = Me.numYStart.Value
+			XmlSettings.SaveToXmlFile()
+		End If
+
+	End Sub
+
+#End Region
+
+#Region "オブジェクトイベント"
+
+	''' <summary>
+	''' テキストボックスドラッグエンター時
+	''' </summary>
+	''' <param name="sender"></param>
+	''' <param name="e"></param>
+	Private Sub txt_DragEnter(sender As Object, e As DragEventArgs) Handles txtCSVFile.DragEnter, txtParentFolder.DragEnter, txtOutputFolder.DragEnter, txtLogFolder.DragEnter
+
+		If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+			e.Effect = DragDropEffects.Copy
+		Else
+			e.Effect = DragDropEffects.None
+		End If
+
+	End Sub
+
+	''' <summary>
+	''' CSVファイルドロップ時
+	''' </summary>
+	''' <param name="sender"></param>
+	''' <param name="e"></param>
+	Private Sub txtCSVFile_DragDrop(sender As Object, e As DragEventArgs) Handles txtCSVFile.DragDrop
+
+		Dim strFiles As String() = CType(e.Data.GetData(DataFormats.FileDrop, False), String())
+		If System.IO.File.Exists(strFiles(0)) Then
+			Me.txtCSVFile.Text = strFiles(0)
+		End If
+
+	End Sub
+
+	''' <summary>
+	''' 親フォルダドロップ時
+	''' </summary>
+	''' <param name="sender"></param>
+	''' <param name="e"></param>
+	Private Sub txtParentFolder_DragDrop(sender As Object, e As DragEventArgs) Handles txtParentFolder.DragDrop
+
+		Dim strFiles As String() = CType(e.Data.GetData(DataFormats.FileDrop, False), String())
+		If System.IO.Directory.Exists(strFiles(0)) Then
+			Me.txtParentFolder.Text = strFiles(0)
+		End If
+
+	End Sub
+
+	''' <summary>
+	''' 出力フォルダドロップ時
+	''' </summary>
+	''' <param name="sender"></param>
+	''' <param name="e"></param>
+	Private Sub txtOutputFolder_DragDrop(sender As Object, e As DragEventArgs) Handles txtOutputFolder.DragDrop
+
+		Dim strFiles As String() = CType(e.Data.GetData(DataFormats.FileDrop, False), String())
+		If System.IO.Directory.Exists(strFiles(0)) Then
+			Me.txtOutputFolder.Text = strFiles(0)
+		End If
+
+	End Sub
+
+	''' <summary>
+	''' ログフォルダドロップ時
+	''' </summary>
+	''' <param name="sender"></param>
+	''' <param name="e"></param>
+	Private Sub txtLogFolder_DragDrop(sender As Object, e As DragEventArgs) Handles txtLogFolder.DragDrop
+
+		Dim strFiles As String() = CType(e.Data.GetData(DataFormats.FileDrop, False), String())
+		If System.IO.Directory.Exists(strFiles(0)) Then
+			Me.txtLogFolder.Text = strFiles(0)
+		End If
+
+	End Sub
+
+	''' <summary>
+	''' 実行ボタン押下時
+	''' </summary>
+	''' <param name="sender"></param>
+	''' <param name="e"></param>
+	Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+
+		If Not System.IO.File.Exists(Me.txtCSVFile.Text) Then
+			MessageBox.Show("CSVファイルが指定されていません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+			Exit Sub
+		End If
+		If Not System.IO.Directory.Exists(Me.txtParentFolder.Text) Then
+			MessageBox.Show("親フォルダが指定されていません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+			Exit Sub
+		End If
+		If Not System.IO.Directory.Exists(Me.txtOutputFolder.Text) Then
+			MessageBox.Show("出力フォルダが指定されていません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+			Exit Sub
+		End If
+		If Not System.IO.Directory.Exists(Me.txtLogFolder.Text) Then
+			MessageBox.Show("ログ保存フォルダが指定されていません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+			Exit Sub
+		End If
+
+		If MessageBox.Show("キャプション挿入処理を開始します" & vbNewLine & "よろしいですか？", "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.No Then
+			Exit Sub
+		End If
+
+		EnableControls(Me, False)
+
+		Dim strCSVFile As String = Me.txtCSVFile.Text
+		Dim strParentFolder As String = Me.txtParentFolder.Text
+		Dim strOutputFolder As String = Me.txtOutputFolder.Text
+		Dim strLogFolder As String = Me.txtLogFolder.Text
+
+		Try
+			Me.lstResult.Items.Clear()
+			WriteLstResult(Me.lstResult, "CSVファイル：" & System.IO.Path.GetFileName(strCSVFile))
+			WriteLstResult(Me.lstResult, "親フォルダ：" & strParentFolder)
+			WriteLstResult(Me.lstResult, "出力フォルダ：" & strOutputFolder)
+			WriteLstResult(Me.lstResult, "ログフォルダ：" & strLogFolder)
+			WriteLstResult(Me.lstResult, "Y軸起点：" & Me.numYStart.Value)
+
+			Dim iRecCount As Integer = 0
+			Using parser As New CSVParser(strCSVFile, System.Text.Encoding.GetEncoding("Shift_JIS"))
+				parser.Delimiter = vbTab    'タブ区切り
+				parser.HasFieldsEnclosedInQuotes = True
+				parser.TrimWhiteSpace = False
+
+				iRecCount = 0
+
+				parser.ReadFields() 'ヘッダ業を読み飛ばす
+				'最終行まで読み込み
+				While Not parser.EndOfData
+					iRecCount += 1
+					Dim row As String() = parser.ReadFields()
+					'子フォルダ以下の画像ファイルをすべて配列に列挙する
+					Dim strFiles As String() = GetFilesMostDeep(strParentFolder & "\" & row(0), {"*.tif"})
+
+				End While
+			End Using
+
+		Catch ex As Exception
+
+			MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+		Finally
+
+			EnableControls(Me, True)
+
+		End Try
+	End Sub
+
+#End Region
+
+End Class
